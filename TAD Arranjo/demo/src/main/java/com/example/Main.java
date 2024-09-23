57997e416aa75a694cfd6a3dc76bf43623a3ce88
Ana Julia <anajcaetanom@gmail.com>
2024-09-22 22:08:52 -0300

    Inicial commit

---
+++ TAD Arranjo/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("oi");
+    }
+}
+
+
+public class TADArranjo {
+
+}
+
+public class Teste {
+
+
+
+
+    public void inserirElemento() {
+
+    }
+
+    public void removerElemento() {
+
+    }
+
+    public void somaParesSimetricos(int k) {
+
+    } 
+
+}
