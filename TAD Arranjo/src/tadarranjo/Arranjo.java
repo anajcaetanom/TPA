cc5a4801b4b18cde6fd05ccc38b50c9be7f85ac2
ana júlia <anajcaetanom@gmail.com>
2024-09-30 16:03:26 -0300

    ordenação por seleção

--- TAD Arranjo/src/tadarranjo/Arranjo.java
+++ TAD Arranjo/src/tadarranjo/Arranjo.java
@@ -1,33 +1,36 @@
 package tadarranjo;
 
+import java.util.Objects;
+
 public class Arranjo {
-    private Object[] items;
+    private Integer[] items;
     private int first, last, position;
 
     // Construtor
     public Arranjo(int maxSize) {
-        this.items = new Object[maxSize];
+        this.items = new Integer[maxSize];
         this.position = -1;
         this.first = 0;
         this.last = this.first;
     }
 
-    // Métodos
+    // Métodos //
 
     public boolean isEmpty() {
         return this.first == this.last;
     }
 
-    public int findIndex (Object item) throws Exception {
+    public int findIndex (Integer item) throws Exception {
         if (this.isEmpty()) {
             throw new IllegalStateException("Empty list.");
         }
+
         if (item == null) {
-            throw new IllegalArgumentException("Invalid item.");
+            throw new IllegalArgumentException("Invalid object.");
         }
 
         for (int i = 0; i < this.last; i++) {
-            if (this.items[i].equals(item)) {
+            if (Objects.equals(this.items[i], item)) {
                 return i;
             }
         }
@@ -35,38 +38,45 @@ public class Arranjo {
         return -1;
     }
 
-    public Object pesquisar (Object item) throws Exception {
+    public int pesquisar (Integer item) throws Exception {
         if (this.isEmpty()) {
             throw new IllegalStateException("Empty list.");
         }
+
         if (item == null) {
-            throw new IllegalArgumentException("Invalid item.");
+            throw new IllegalArgumentException("Invalid object.");
         }
+
         for (int i = 0; i < this.last; i++) {
-            if (this.items[i].equals(item)) {
+            if (Objects.equals(this.items[i], item)) {
                 return this.items[i];
             }
         }
-        return null;
+
+        throw new Exception("Element not found");
     }
 
-    public void inserir (Object item) throws Exception {
+    public void inserir (Integer item) throws Exception {
+        if (item == null) {
+            throw new IllegalArgumentException("Invalid object.");
+        }
         if (this.last >= this.items.length) {
             throw new Exception("The list is full.");
-        } else if (this.pesquisar(item) == item) {
-            throw new Exception("Object already on the list.");
-        } else {
-            this.items[this.last] = item;
-            this.last++;
         }
+        if (this.pesquisar(item) == item) {
+            throw new Exception("Object already on the list.");
+        }
+        this.items[this.last] = item;
+        this.last++;
     }
 
-    public Object remover (Object item) throws Exception {
+    public void remover (Integer item) throws Exception {
         if (this.isEmpty()) {
             throw new IllegalStateException("Empty list.");
         }
+
         if (item == null) {
-            throw new IllegalArgumentException("Invalid item.");
+            throw new IllegalArgumentException("Invalid object.");
         }
 
         int index = this.findIndex(item);
@@ -75,40 +85,41 @@ public class Arranjo {
             throw new Exception("Item not found.");
         }
 
-        Object removed_item = this.items[index];
-
         this.last--;
 
-        for (int j = index; j < this.last; j++) {
+        for (int j = index; j < this.last - 1; j++) {
             this.items[j] = this.items[j + 1];
         }
 
         this.items[this.last] = null;
 
-        return removed_item;
     }
 
     public int somaParesSimetricos (int k) throws Exception {
         if (this.isEmpty()) {
             throw new IllegalStateException("Empty list.");
         }
+        int len = this.items.length;
+        int sum = this.items[k] + this.items[len - k];
 
-
-
-    }
-}
-
-class QuickSort {
-
-    private static class LimiteParticoes {
-        int i, j;
+        return sum;
     }
 
-    private static LimiteParticoes partition(Object[] items, int left, int right) {
-        LimiteParticoes lp = new LimiteParticoes();
-        lp.i = left;
-        lp.j = right;
+    public static void ordenacaoSelecao (Integer[] items, int tam) {
+        for (int i = 1; i < tam; i++) {
+            int min = i;
+            for (int j = i + 1; j <= tam; j++) {
+                if (items[j] < items[min]) {
+                    min = j;
+                }
+            }
+
+            Integer aux = items[min];
+            items[min] = items[i];
+            items[i] = aux;
+        }
     }
+
 }
 
 class Teste {
